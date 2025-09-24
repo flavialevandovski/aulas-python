e86a4c28ae8c6b6be804a23232584912995f2e5f
Maykon Granemann <dyego.maykon@luizalabs.com>
2025-09-24 19:20:47 -0300

    Aula2-Print e exercicios

---
+++ Aula002/aula002-1-print.py
@@ -0,0 +1,12 @@
+#--- Aula 2  24-09-2025
+#--- Python e aplicação console - Função Print
+
+
+#--- Impressão de texto simples
+print('Olá Mundo!')
+
+#--- Impressão de mais de um texto no mesmo print
+print('Olá', 'Flavia', 'Seja bem vind')
+
+#--- Impressão de texto e números
+print('Olá', 'Flavia', 'Seja bem vinda', 'Você tem', 22, 'anos')
---
+++ Aula002/aula002-2-print.py
@@ -0,0 +1,34 @@
+#--- Aula 2  24-09-2025
+#--- Python e aplicação console - Função Print
+
+#--- Impressão de texto em multiplas linhas com caractere de escape \n
+print('Olá, Flavia!\nSeja bem-vinda')
+
+#--- Impressão de texto em multiplas linhas usando aspas triplas
+print('''Olá, Flavia!
+Seja bem-vinda''')
+
+#--- Impressão de texto com tabulação com caractere de escape \t
+print('Olá, Flavia!\tSeja bem-vinda')
+
+#--- Impressão de texto com aspas simples com caractere de escape \'
+print('Olá, Flavia!\'Seja bem-vinda\'')
+
+#--- Impressão de texto com aspas duplas com caractere de escape \"
+print("Olá, Flavia!\"Seja bem-vinda\"")
+
+#--- Impressão de texto com barra invertida com caractere de escape \\
+print('Olá, Flavia!\\Seja bem-vinda\\')
+
+#--- Impressão de texto com caractere de escape \r para retorno
+print('Olá, Flavia!\rSeja bem-vinda')
+
+#--- Impressão de texto com caractere de escape \v para vertical tab
+print('Olá, Flavia!\vSeja bem-vinda\vao nosso sistema')
+
+#--- Impressão de texto com caractere de escape \b para backspace
+print('Olá, Flavia!\b Seja bem-vinda')
+
+#--- Impressão com multiplicação de caracteres
+barra_cabecalho = '='*10
+print(barra_cabecalho,'Sistema da Flavinha',barra_cabecalho)
