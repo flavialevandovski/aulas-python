2060a8b44efcb6b9c50b90456cc761daa234e763
Flavia Levandovski <flaviaeduardamachado381@gmail.com>
2025-09-23 22:53:23 -0300

    codigos da aula 1:print,input,variaveis

---
+++ aula 1/aula1.py
@@ -0,0 +1,5 @@
+print("eu amo o meu amor","Maykon", "pentelho")
+# salvando o nome digitado pelo usuario
+nome = input("digite seu nome:")
+# Imprimindo uma mensagem de boas-vindas + o nome salvo do usuario
+print("bem-vindo,", nome, "acesse nosso sistema")
---
+++ aula 1/ex1.py
@@ -0,0 +1,12 @@
+#exercicio 1
+# Crie um sistema de cadastro de usuarios:
+# O sistema deve ter uma mensagem de boas vindas
+# Solicite para que usuario digite seu nome, sobrenome e idade
+# salve cada informacao em uma variavel 
+# Ao final imprima todas as informcoes digitas pelo usuario no seguinte formato
+# Usuario cadastrado! Nome: Maykon Sobrenome: Granemann Idade:18
+print("ben-vindo ao nosso sistema")
+nome = input("digite seu nome:")
+sobrenome = input("digite seu sobrenome:")
+idade = input("digite sua idade:")
+print("usuario cadastrado! nome:",nome, "Sobrenome:",sobrenome, "idade:", idade)
