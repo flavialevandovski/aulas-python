b08909228694558f67dcfdb2b00faf331c9e58cf
Maykon Granemann <dyego.maykon@luizalabs.com>
2025-10-01 22:40:42 -0300

    aula 003- Variáveis e exercícios

---
+++ Aula003/aula003-variaveis.py
@@ -0,0 +1,24 @@
+#--- Aula 2  01-10-2025
+#--- Variáveis = Espaço na memoria do computador, usamos um apelido para nos referirmos a esse espaço.
+
+#--- Variável numérica do tipo inteiro - int
+idade = 22
+
+#--- Variável numérica de tipo ponto flutuante - float
+salario = 1950.15
+
+#--- Variável texto - str
+nome = 'Flavia'
+
+#--- Variáveis do tipo booleana - bool
+verdadeiro = True
+falso = False
+
+#--- Impressão de multiplos textos e variáveis usando virgula
+print('Nome:', nome, 'Idade:', idade, 'Salario:', salario, 'Verdadeiro:', verdadeiro, 'Falso:', falso)
+
+#--- Impressão de multiplos textos e variáveis usando a função format
+print('Nome: {} Idade: {} Salario: {} Verdadeiro: {} Falso: {}'.format(nome, idade, salario, verdadeiro, falso))
+
+#--- Impressão de multiplos textos e variáveis usando interpolação de strings - FStrings
+print(f'Nome: {nome} Idade: {idade} Salario: {salario} Verdadeiro: {verdadeiro} Falso: {falso}')
