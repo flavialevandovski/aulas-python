e1cc70fc2060137f1c2831b06013325e97eef1f6
Flavia Levandovski <flaviaeduardamachado381@gmail.com>
2025-10-02 14:36:16 -0300

    atividade 3- aula 03

--- Aula003/exercicios/exercicio3.py
+++ Aula003/exercicios/exercicio3.py
@@ -4,3 +4,22 @@
 #--- A impressão deve conter informações do livro, que deverão estar em variáveis
 #--- Livro: Título, Edição, Autor, Data de publicação
 #--- Os parágrafos devem estar formatados conforme a formatação do livro
+barra_cabecalho= '='*10
+print('\n'*1)
+print(barra_cabecalho,'livro',barra_cabecalho)
+print('\n'*1)
+titulo='transformando palavras em dinehiro'
+ediçao= '1° edição'
+autor='icáro de carvalho'
+data='3 de fevereiro de 2020'
+print(f'titulo= {titulo}')
+print(f'ediçao= {ediçao}')
+print(f'autor= {autor}')
+print(f'data= {data}')
+print('\n'*1)
+print(barra_cabecalho,'paragrafo 1',barra_cabecalho)
+print(''' Escrever é mais do que juntar palavras.\n É transmitir idéias de forma clara e persuasiva''')
+print('\n'*1)
+print(barra_cabecalho,'paragrafo 2',barra_cabecalho )
+print(''' Quando aprendemos a usar as palavras como instrumento de valor,\n descobrimos que elas transforman a forma como as pessoas nos enxerga''')
+print('\n'*1)
