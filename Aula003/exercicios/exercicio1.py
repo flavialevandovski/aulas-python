d88969d7f497fb2b798340a67a3e37ce75c3e312
Flavia Levandovski <flaviaeduardamachado381@gmail.com>
2025-10-02 00:40:55 -0300

    esxerciocio 1- aula 03

--- Aula003/exercicios/exercicio1.py
+++ Aula003/exercicios/exercicio1.py
@@ -1,7 +1,12 @@
 #--- Exercicio 1  - Variávies e impressão com interpolacão de string
-
 #--- Crie 5 variávies para armazenar os dados de um funcionário
 #--- Funcionario: Nome, Sobrenome, Cpf, Rg, Salário
 #--- Os dados devem ser impressos utilizando a funcao format()
 #--- Deve ser usado apenas uma vez a função print(), porem os dados devem ser apresentados um em cada linha
 #--- O salario deve ser de tipo flutuante e na impressão deve apesentar apenas duas casas após a vírgula
+nome= 'Flavia'
+sobrenome= 'Levandovski'
+cpf= '135.010.309-83'
+rg= '7.948.616'
+salario= 3000.03
+print ('nome: {} \nsobrenome: {} \nCpf {} \nRg: {} \nsalario: {:.2f}\n'.format(nome,sobrenome,cpf,rg,salario))
